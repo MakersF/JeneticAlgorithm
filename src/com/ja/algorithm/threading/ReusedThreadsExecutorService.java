664703466d90abb710b1ec254d68aeca72dbd660
Francesco Zoffoli <makers.f@gmail.com>
2015-08-18 15:43:52 +0200

    Added a ExecutorService to use the WorkerThread we implemented. The goal is not to emulate a FixedThreadPool, but reusing always the same threads and not spawn new one if the previous one end up without work

---
+++ src/com/ja/algorithm/threading/ReusedThreadsExecutorService.java
@@ -0,0 +1,92 @@
+package com.ja.algorithm.threading;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class ReusedThreadsExecutorService extends AbstractExecutorService {
+
+	private final WorkerThread[] threads;
+	private final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<Runnable>();
+	private boolean shutdown = false;
+	private boolean terminated = false;
+
+	public ReusedThreadsExecutorService(int numberOfThreads) {
+		threads = new WorkerThread[numberOfThreads];
+		for(int i=0; i < numberOfThreads; i++) {
+			threads[i] = new WorkerThread(tasks);
+			threads[i].start();
+		}
+	}
+	@Override
+	public void execute(Runnable command) {
+		if(!shutdown) {
+			while(true) {
+				try {
+					tasks.put(command);
+				} catch (InterruptedException e) {
+					continue;
+				}
+			}
+		}
+	}
+
+	@Override
+	public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+		if(terminated)
+			return true;
+		long remaining = unit.toNanos(timeout);
+		for(WorkerThread wt : threads) {
+			long old = System.nanoTime();
+			if(!wt.awaitTermination(remaining, TimeUnit.NANOSECONDS))
+				return false;
+			long now = System.nanoTime();
+			remaining -= now - old;
+			if(remaining < 0 ) {
+				return false;
+			}
+		}
+		terminated = true;
+		return true;
+	}
+
+	@Override
+	public boolean isShutdown() {
+		return shutdown;
+	}
+
+	@Override
+	public boolean isTerminated() {
+		if(terminated)
+			return true;
+		if(!shutdown)
+			return false;
+
+		for(WorkerThread wt : threads) {
+			if(!wt.isTerminated())
+				return false;
+		}
+		terminated = true;
+		return terminated;
+	}
+
+	@Override
+	public void shutdown() {
+		shutdown = true;
+	}
+
+	@Override
+	public List<Runnable> shutdownNow() {
+		shutdown();
+		for(WorkerThread wt : threads) {
+			wt.shutdown();
+		}
+
+		List<Runnable> remaining_tasks = new ArrayList<Runnable>(tasks.size());
+		tasks.drainTo(remaining_tasks);
+		return remaining_tasks;
+	}
+}
